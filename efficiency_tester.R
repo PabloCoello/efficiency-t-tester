8a9b6a9178557c77958abe264caaae99018b2ec4
Pablo C <pablo.coello@outlook.es>
2019-09-09 10:21:58 +0200

    Added wilcox test

--- efficiency_tester.R
+++ efficiency_tester.R
@@ -1,4 +1,5 @@
 library(readxl)
+library(readr)
 library(stats)
 rm(list=ls())
 
@@ -32,42 +33,57 @@ get_t_test_pvalue = function(reference, x, confidence){
 }
 
 
-get_t_test_periods_match = function(pvalues_array, periods, confidence){
+get_test_periods_match = function(pvalues_array, periods, confidence){
   periods = pvalues_array[1:periods]
   match = length(periods[which(periods>(1-confidence))])/length(periods)
   return(match)
 }
 
 
-get_t_test_dmu_match = function(pvalues_array, dmu, confidence){
+get_test_dmu_match = function(pvalues_array, dmu, confidence){
   dmu_pvalues = pvalues_array[c((length(pvalues_array)-dmu):length(pvalues_array))]
   match = length(dmu_pvalues[which(dmu_pvalues>(1-confidence))])/length(dmu_pvalues)
   return(match)
 }
 
+get_wilcox_test_pvalue = function(reference, x, confidence){
+  array=matrix(nrow=1, ncol=ncol(reference)+nrow(reference))
+  for (i in 1:ncol(reference)){
+    wilcox=wilcox.test(x=reference[,i], y=x[,i], conf.level = confidence)
+    array[,i]=wilcox$p.value
+  }
+  for (j in 1:nrow(reference)){
+    wilcox=wilcox.test(x=reference[j,], y=x[j,], conf.level = confidence)
+    array[,ncol(reference)+j]=wilcox$p.value
+  }
+  return(array(array))
+}
+
 get_result_matrix = function(df, reference, confidence, periods, dmu){
-  result_matrix = data.frame(matrix(ncol = ncol(data), nrow = 2), row.names = c("periods match", "dmu match"))
-  colnames(result_matrix)=names(data)
+  result_matrix = data.frame(matrix(ncol = 4, nrow = ncol(data)))
+  rownames(result_matrix)=names(data)
+  colnames(result_matrix) = c("t.test periods match", "t.test dmu match",
+                "wilcox.test periods match"," wilcox.test dmu match")
   for (i in names(data)){
     pvalues_array = get_t_test_pvalue(reference, x=get(i), confidence)
-    result_matrix[1,i] = get_t_test_periods_match(pvalues_array, periods, confidence)
-    result_matrix[2,i] = get_t_test_dmu_match(pvalues_array, dmu, confidence)
+    result_matrix[i,1] = get_test_periods_match(pvalues_array, periods, confidence)
+    result_matrix[i,2] = get_test_dmu_match(pvalues_array, dmu, confidence)
+    pvalues_array = get_wilcox_test_pvalue(reference, x=get(i), confidence)
+    result_matrix[i,3] = get_test_periods_match(pvalues_array, periods, confidence)
+    result_matrix[i,4] = get_test_dmu_match(pvalues_array, dmu, confidence)
   }
   return(result_matrix)
 }
 
 setwd("C:/Users/epiph/OneDrive - Universidade de Santiago de Compostela/Proyecto MyCoast/Datos procesados")
-panel_data<- read_excel("panel_data_2.xlsx")
-data = panel_data[,c(127,128,129,130,131)]
+data <- read_excel("efficiency_matrix.xlsx")
 
 
 for (i in 1:length(data)){
   assign(names(data)[i], split_in_variables(df=data[,i], periods=120, dmu=28))
 }
-result_matrix = get_result_matrix(df=data, reference=teradial_prod_crs, confidence = 0.95, periods = 120, dmu=28)
+(result_matrix_total_traffic = get_result_matrix(df=data, reference=tbc_crs_wages_R, confidence = 0.95, periods = 120, dmu=28))
 
 
-
-
-
-
+#write.csv2(x=result_matrix_fish,file="matrix_liquid_fish.csv")
+#t.test(x=COBB_c[,1],y=teradialbc_prod_crs_hetero[,1])
